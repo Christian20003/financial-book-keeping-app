e6949adf2f2299a363854798af25513436853d72
Christian20003 <christian.goellner@bnv-bamberg.de>
2024-03-25 15:46:54 +0100

    refactor: Improved the Loading-Component - in SCSS and TS and HTML-template

--- src/app/shared/components/loading/loading.component.ts
+++ src/app/shared/components/loading/loading.component.ts
@@ -1,8 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-loading',
   templateUrl: './loading.component.html',
   styleUrls: ['./loading.component.scss'],
 })
-export class LoadingComponent {}
+export class LoadingComponent implements OnInit {
+  /* This array saves the IDs of all shown icons */
+  icons: Array<{ id: string }> = [];
+  /* The number of icons which should be displayed */
+  number = 4;
+
+  ngOnInit(): void {
+    // Create the IDs
+    for (let i = 0; i <= this.number; i++) {
+      this.icons.push({ id: 'icon-' + i });
+    }
+  }
+}
